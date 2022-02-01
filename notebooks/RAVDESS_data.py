11a7343791f61ca5667a6a44722285c07370a48f
Stephen William <swilliam.productions@gmail.com>
2022-01-31 19:56:13 -0500

    added paths

--- notebooks/RAVDESS_data.py
+++ notebooks/RAVDESS_data.py
@@ -1,2 +1,20 @@
 # custom Pytorch Data Class for the RAVDESS Dataset
 
+import pandas as pd 
+import numpy as np 
+import torch 
+import torchaudio
+from torch.utils.data import Dataset
+
+# Global variables 
+
+SAMPLE_RATE = 16000
+N_FFT = int(0.025 * SAMPLE_RATE)   # 25 ms 
+HOP_LENGTH = int(0.01 * SAMPLE_RATE)  # 10 ms
+N_MELS = 13
+
+# Dataset Paths 
+
+RAVDESS_dir = '/Users/stephen/Desktop/Emotion_Speech_Detection/data/RAVDESS'
+RAVDESS_metadata = '/Users/stephen/Desktop/Emotion_Speech_Detection/data/metadata.csv'
+
